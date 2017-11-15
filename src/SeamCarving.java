876ccb4c2b9ba0c3949038439b71d311cc416662
Chris Wailes <chris.wailes@gmail.com>
2017-11-15 13:55:18 -0500

    Initial commit of student handout code.

---
+++ src/SeamCarving.java
@@ -0,0 +1,7 @@
+public class SeamCarving {
+
+	// TODO: Find the seam with the lowest disruption measure
+	public static Seam carve_seam(int[][] disruption_matrix) {
+		throw new UnsupportedOperationException();
+	}
+}
